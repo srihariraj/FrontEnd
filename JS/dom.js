20a361001e4970928acb0ff4782791620318f9fe
srihariraj <srihariatom@gmail.com>
2024-07-30 19:26:54 +0530

    css advance

---
+++ JS/dom.js
@@ -0,0 +1,6 @@
+function doSomething(){
+	let x = document.getElementById("h-one");
+	x.innerHTML="<i>Hello</i>";
+	document.getElementById("h-one").style.color='orange';
+	x.style.display='block';
+}
